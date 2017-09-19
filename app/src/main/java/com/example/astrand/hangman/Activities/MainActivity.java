ae326d22dd1e8dde7518a5c0d3bc84d36b4c6e86
Andreas Strand <strand118@hotmail.com>
2017-09-19 12:57:01 +0200

    Deleted Language class

--- app/src/main/java/com/example/astrand/hangman/Activities/MainActivity.java
+++ app/src/main/java/com/example/astrand/hangman/Activities/MainActivity.java
@@ -10,7 +10,6 @@ import android.view.View;
 import android.widget.ImageView;
 
 import com.beardedhen.androidbootstrap.BootstrapButton;
-import com.example.astrand.hangman.ModelSettings.Language;
 import com.example.astrand.hangman.R;
 
 import java.util.Locale;
